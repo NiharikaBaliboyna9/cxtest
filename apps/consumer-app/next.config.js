21cc0a31d12d1ca257ce6babdc01bbd1aaefece0
NiharikaBaliboyna9 <balibon@amazon.com>
2024-08-27 16:00:09 +0530

    Update next.config.js

--- apps/consumer-app/next.config.js
+++ apps/consumer-app/next.config.js
@@ -1,28 +1,11 @@
-/** @type {import('next').NextConfig} */
-
+const path = require('path');
 const nextConfig = {
-	images: {
-		dangerouslyAllowSVG: true,
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '*',
-			},
-			// TODO: Add further security by granular whitelisting
-		],
-	},
-	reactStrictMode: true,
-	async redirects() {
-		return [
-			{
-				source: '/sg',
-				destination: '/',
-				permanent: true,
-			},
-		]
-	},
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),           // Here you can modify as per your use case. Currently, the NextJS folder is two folders inside from the root folder, you can modify the same for one folder.
+  }
 }
-
-module.exports = {
-	nextConfig,
-}
+module.exports = nextConfig
